a6f6bdb0e5a71ea965abcb22dc17f30957cc2e77
Taran Arora <taranarora@pop-os.localdomain>
2020-08-21 00:04:09 +0530

    added footer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Home.css";
 import ListingGrid from "../ListingsGrid/ListingsGrid";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 import data from "../../data.json";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -9,13 +11,30 @@ function Home() {
       <div className="home__cover">
         <div className="home__container">
           <h1>Your favorite food, cooked by Snackstore</h1>
-          {/* <div className="home__options">
-            <input type="text" className="home__search" />
+          <div className="home__options">
+            <input
+              type="text"
+              placeholder="Search for your favorite food"
+              className="home__search"
+            />
             <button>Find Food</button>
-          </div> */}
+          </div>
         </div>
       </div>
       {data && data.map((item, i) => <ListingGrid key={i} data={item} />)}
+      <div className="home__footer">
+        <div className="home__footerLogo">
+          <Link to="/" className="footer__logo">
+            <span className="footer__logoFirst">Snack</span>
+            <span className="footer__logoSecond">Store</span>
+          </Link>
+        </div>
+        <div className="home__footerleft">
+          <span>
+            Built with <FavoriteIcon /> by Taran Arora
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
